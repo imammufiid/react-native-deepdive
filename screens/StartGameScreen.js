db17351a9a8cd68b456ec5bb15013aaf52cc1763
imufiid <imufiid98@gmail.com>
2023-11-26 06:42:01 +0700

    feat: Created starting project

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import {View, TextInput, StyleSheet} from "react-native";
+
+export const StartGameScreen = () => {
+    return (
+        <View>
+            <TextInput/>
+            {/*Button Component*/}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
